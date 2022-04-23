9ee10d8fa3a2fe0bf9f51a4816f02a5a57d2e9e3
bravoarthur <arthur.bravo08@gmail.com>
2022-04-23 19:11:40 +1000

    Done 8-ts

---
+++ src/pages/Cardapio/Itens/index.tsx
@@ -0,0 +1,72 @@
+import { useEffect } from 'react';
+import { useState } from 'react';
+import Item from './Item';
+import itens from './itens.json'
+import styles from './Itens.module.scss'
+
+
+interface Props {
+
+    busca: string;
+    filtro: number | null;
+    ordenador: string;
+}
+
+
+
+function Itens(props: Props) {
+    
+    const [lista, setLista] = useState(itens)
+    const {busca, filtro, ordenador} = props
+    
+
+   
+
+    useEffect(()=> {
+
+        function testaBusca(title: string) {
+            const regex = new RegExp(busca, 'i')
+    
+            return regex.test(title)
+    
+        }
+    
+        function testaFiltro (id: number) {
+            if(filtro !== null) return filtro === id;
+            return true
+        }
+    
+        function ordenar (novaLista: typeof itens) {
+    
+            switch(ordenador) {
+                case 'porcao':
+                    return novaLista.sort((a, b) => a.size > b.size ? 1 : -1)
+                case 'qtd_pessoas': 
+                    return novaLista.sort((a, b) => a.serving > b.serving ? 1 : -1)
+                case 'preco':
+                    return novaLista.sort((a, b) => a.price > b.price ? 1 : -1)
+                default:
+                    return novaLista
+            }
+        }
+
+        const novaLista = itens.filter((item) => testaBusca(item.title) && testaFiltro(item.category.id))
+
+        setLista(ordenar(novaLista))
+    
+    }, [busca, filtro, ordenador])
+    
+    return ( 
+
+        <div className={styles.itens}>
+            {lista.map((item) => (
+                
+                <Item key={item.id} {...item}/>
+                
+            ))}
+        </div>
+     );
+}
+
+export default Itens;
+
--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,17 +1,46 @@
-import React from 'react';
-import styles from './Cardapio.module.scss'
 
+import styles from './Cardapio.module.scss'
+//import logo from '../../assets/logo.svg' para usar como src de um img
+import {ReactComponent as Logo} from "assets/logo.svg"
+import Buscador from './Buscador'
+import { useState } from 'react'
+import Filtros from './Filtros'
+import Ordenador from './Ordenador'
+import Itens from './Itens'
 
 export default function Cardapio () {
 
-
+    const [busca, setBusca] = useState('')
+    const [filtro, setFiltro] = useState<number | null>(null)
+    const [ordenador, setOrdenador] = useState('')
 
     return (
 
         <main>
-            <nav className='menu'>
-
+            <nav className={styles.menu}>
+                <Logo/>
+                
             </nav>
+            <header className={styles.header}>
+                <div className={styles.header__text}>
+                    A Casa do Codigo e da massa
+
+                </div>
+
+            </header>
+
+            <section className={styles.cardapio}>
+                <h3 className={styles.carpio__titulo}>Cardapio</h3>
+                <Buscador busca={busca} setBusca={setBusca}/>
+                <div className={styles.cardapio__filtros}>
+
+                    <Filtros filtro={filtro} setFiltro={setFiltro}/>
+                    <Ordenador ordenador={ordenador} setOrdenador={setOrdenador}/>
+                    
+                </div>
+                <Itens busca={busca} filtro={filtro} ordenador={ordenador}/>
+
+            </section>
         </main>
 
     )
