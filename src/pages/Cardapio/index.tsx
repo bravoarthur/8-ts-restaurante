b9410d7e63885f3a66c4b3e41540f5ef23985c1a
bravoarthur <arthur.bravo08@gmail.com>
2022-04-23 13:26:54 +1000

    start

---
+++ src/pages/Cardapio/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from './Cardapio.module.scss'
+
+
+export default function Cardapio () {
+
+
+
+    return (
+
+        <main>
+            <nav className='menu'>
+
+            </nav>
+        </main>
+
+    )
+
+}
